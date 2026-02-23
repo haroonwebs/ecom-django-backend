c8c35db5310acf481871246dc7090f0c684f75db
haroontalib <haroontalib8029@gmail.com>
2026-02-24 01:43:42 +0500

    fixed some issue and added pagination

--- project/products/urls.py
+++ project/products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProductListView, CreateProductView, UpdateProductView
+from .views import ProductListView, CreateProductView, UpdateProductView, GetSingleProductView, DeleteProductView
 
 
 urlpatterns=[
     path('products/', ProductListView.as_view(), name='produts'),
     path('create-product/', CreateProductView.as_view(), name='create-product'),
-    path('<int:pk>/update/', UpdateProductView.as_view(), name='update-product')
+    path('<int:pk>/update/', UpdateProductView.as_view(), name='update-product'),
+    path('<int:pk>/', GetSingleProductView.as_view(), name='get-single-product'),
+    path('<int:pk>/delete/', DeleteProductView.as_view(), name='delete-product')
 ]
--- project/products/views.py
+++ project/products/views.py
@@ -10,11 +10,22 @@ from rest_framework.permissions import IsAdminUser
 class ProductListView(APIView):
     renderer_classes=[DataRenderer]
     def get(self, request, format=None):
-        prodcuts=Product.objects.all()
-        serializer= ProductSerializer(prodcuts, many=True)
+        page = int(request.GET.get('page', 1))
+        limit = int(request.GET.get('per_page', 10))
+
+        start = (page - 1) * limit
+        end = start + limit
+        products=Product.objects.all().order_by('-created_at')[start:end]
+
+        serializer= ProductSerializer(products, many=True)
+
+        total_products = Product.objects.count()
+        total_pages = (total_products + limit - 1) // limit  
         return Response({
             'message':'Products Fetched',
-            'data': serializer.data
+            'data': serializer.data,
+            "total_pages": total_pages,
+            "current_page": page
         }, status=status.HTTP_200_OK)
 
 
@@ -36,16 +47,48 @@ class CreateProductView(APIView):
 # update product view    
 class UpdateProductView(APIView):
     def patch(self, request, pk ,format=None):
-        product=Product.objects.get(pk=pk)
-        if product is not None:
-            serializer=ProductSerializer(product, data=request.data, partial=True)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response({
-                    'message':'Product updated Successfully',
-                    'data': serializer.data
-                }, status=status.HTTP_200_OK)
+        try:
+            product=Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer=ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({
+                'message':'Product updated Successfully',
+                'data': serializer.data
+            }, status=status.HTTP_200_OK)
+        else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+# get single product view    
+class GetSingleProductView(APIView):
+    renderer_classes=[DataRenderer]
+    def get(self, request, pk ,format=None):
+        product=''
+        try:
+            product=Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer=ProductSerializer(product)
         return Response({
-            'message':'Product Not Found'
-        }, status=status.HTTP_404_NOT_FOUND)
+            'message':'Product fetched Successfully',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)   
+    
+
+# delete product
+class DeleteProductView(APIView):
+    renderer_classes=[DataRenderer]
+    def delete(self, request, pk ,format=None):
+        try:
+            product=Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer=ProductSerializer(product)
+        product.delete()
+        return Response({
+            'message':'Product deleted Successfully',
+        }, status=status.HTTP_200_OK)   
